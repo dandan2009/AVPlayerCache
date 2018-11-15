df8b4884b79849b12453a35e6474f961af35cd9b
Karol Wawrzyniak <karol.wawrzyniak@fivedottwelve.com>
2018-11-15 09:25:32 +0100

    addad AVPlayerCache

---
+++ AVPlayerCache/AVPlayerCache/AVPlayerCache.h
@@ -0,0 +1,19 @@
+//
+//  AVPlayerCache.h
+//  AVPlayerCache
+//
+//  Created by Karol Wawrzyniak on 15/11/2018.
+//  Copyright © 2018 KAWA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AVPlayerCache.
+FOUNDATION_EXPORT double AVPlayerCacheVersionNumber;
+
+//! Project version string for AVPlayerCache.
+FOUNDATION_EXPORT const unsigned char AVPlayerCacheVersionString[];
+#import "AVPlayerItem+MCCacheSupport.h"
+// In this header, you should import all the public headers of your framework using statements like #import <AVPlayerCache/PublicHeader.h>
+
+
